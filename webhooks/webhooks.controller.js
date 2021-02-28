2699b7c69a678c07a3563e7f50a43afc6e0f589a
Robert <robertbellorin.work@gmail.com>
2021-02-28 12:37:22 -0500

    fixed issue

--- webhooks/webhooks.controller.js
+++ webhooks/webhooks.controller.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const productScopes = [
   { scope: "store/product/created", destination: "/webhooks/products/created" },
-  { scope: "store/product/created", destination: "/webhooks/products/updated" },
+  { scope: "store/product/updated", destination: "/webhooks/products/updated" },
 ];
 
 // storeA created URL
@@ -17,32 +17,9 @@ const productScopes = [
 
 module.exports = async () => {
  
-  // For Store A
-  await BigCommerceStoreA.delete("/hooks").then((data) => {
-    const webhooks = data.data;
-    console.log(webhooks);
-    const scopes = webhooks.map((a) => a.scope);
-    if (
-      scopes.indexOf("store/product/created") > -1 ||
-      scopes.indexOf("store/product/updated") > -1
-    ) {
-      console.log("Product webhook already exists");
-    } else {
-      productScopes.forEach((el) => {
-        const hookBody = {
-          scope: el.scope,
-          destination: `${process.env.APP_URL}${el.destination}-storeA`,
-          is_active: true,
-        };
-        console.log(hookBody);
-        BigCommerceStoreA.post("/hooks", hookBody).then((data) => {
-          console.log("Product webhook created");
-        });
-      });
-    }
-  });
   BigCommerceStoreA.get("/hooks").then((data) => {
     const webhooks = data.data;
+    console.log(webhooks);
     const scopes = webhooks.map((a) => a.scope);
     if (
       scopes.indexOf("store/product/created") > -1 ||
